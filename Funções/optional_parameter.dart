38e7b05d1d653d0a50d20342a433a41525774c47
Jimmy Castilho <142106079+jimmyadmsenior@users.noreply.github.com>
2025-02-19 08:22:58 -0300

    novas alterações

--- Funções/optional_parameter.dart
+++ Funções/optional_parameter.dart
@@ -1,10 +1,8 @@
-int soma({int num1 = 5})
+int soma({int num1 = 5, int num2 = 6}) {
+  return num1 + num2;
+}
 
-
-
-
-
-void main(){
+void main() {
   int result = soma();
-  print("O resultado da some é: $result")
+  print("O resultado da some é: $result");
 }
